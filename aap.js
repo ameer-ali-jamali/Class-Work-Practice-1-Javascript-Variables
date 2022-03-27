e41511e1cd3c678742eabaed1524eeb060cfc9b0
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-27 17:57:51 +0500

    Javascript-if-else-practice

---
+++ aap.js
@@ -0,0 +1,32 @@
+var num = prompt("Enter any Number")
+document.write(num%2==0)
+if(num%2==0){
+alert("Even")
+
+}
+else{
+alert("odd")
+}
+var a = 8
+let b = 9
+const c = 10
+{
+    var a = 8
+    let b = 5
+    // alert(a>6 && b<8 )s
+    alert(a>6 || b<8)
+    }
+var num1 = prompt();
+var num2 = prompt();
+if (num1>num2) {
+  alert(num1);
+} 
+else {
+  alert(num2);
+}
+if (num2 > num1) {
+  alert(num2);
+} 
+else {
+  alert(num1);
+}
